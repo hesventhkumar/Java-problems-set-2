f323543f65e2cb30fbf14ee4317f2de639d31f14
hesventhkumar <141727454+hesventhkumar@users.noreply.github.com>
2024-06-12 11:19:58 +0530

    Create find the factor of given number.java

---
+++ find the factor of given number.java
@@ -0,0 +1,14 @@
+class Main{
+public static void main(String args[]){
+long N,fact;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextLong();
+        for(fact=1;fact<=N/2;fact++){
+            if(N%fact==0){
+                System.out.print(fact+" ");
+            }
+//            System.out.print(N+" ");
+        }
+
+    }
+}
